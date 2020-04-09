6bd8b0259e35e918dfafa15cfd38e8b301d650e8
Josh Dick <josh@joshdick.net>
2020-04-09 16:30:09 +0000

    Stop tracking compiled TypeScript (dist/*) in Git

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.POST_TAG_STYLES = Object.freeze({
-    SPACE_DELIMITED: Symbol('tag_style:space_delimited'),
-    YAML_LIST: Symbol('tag_style:yaml_list'),
-});
-//# sourceMappingURL=constants.js.map
